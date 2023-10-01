a9010216191c65e1c88880578ec582e28f48dfc8
JakubSapko <jakub.sapko1@gmail.com>
2023-10-01 16:47:31 +0200

    messing around with click

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ def cli():
 
 
 @cli.command()
-def initdb():
+@click.option(
+    "--anon",
+    help="Show your current config. Use --anon to anonymize your password",
+    is_flag=True,
+)
+def config(anon: bool):
+    if anon:
+        click.echo("Your config is: ...")
     click.echo("Initialized the database")
 
 
 @cli.command()
-def dropdb():
+@click.argument("val")
+def set_bar(val):
     click.echo("Dropped the database")
-
-
-if __name__ == "__main__":
-    cli()
