4e39d324740a2e90a27724716c627791861a57da
JakubSapko <jakub.sapko1@gmail.com>
2023-10-03 17:25:53 +0200

    add: config setup

--- etae/main.py
+++ etae/main.py
@@ -2,13 +2,16 @@ import click
 import platform
 from etae.utils.message_utils import format_config_message
 
-from etae.utils.utils import get_config_file
+from etae.utils.utils import get_config_file, set_config_file
 
 
 @click.group(invoke_without_command=True)
 @click.pass_context
 def cli(ctx):
-    pass
+    current_system: str = platform.system()
+    ctx.ensure_object(dict)
+    ctx.obj["system"] = current_system
+    click.echo("Welcome to etae!")
 
 
 @cli.command()
@@ -17,8 +20,9 @@ def cli(ctx):
     help="Show your current config. Use --anon to anonymize your password",
     is_flag=True,
 )
-def get_config(anon: bool):
-    current_system: str = platform.system()
+@click.pass_context
+def get_config(ctx, anon: bool):
+    current_system: str = ctx.obj["system"]
     config = get_config_file(current_system)
     formatted_message: str = format_config_message(config, anon)
     click.echo("Current config:")
@@ -26,6 +30,16 @@ def get_config(anon: bool):
 
 
 @cli.command()
-@click.argument("val")
-def set_config(val):
-    click.echo("Dropped the database")
+@click.pass_context
+def set_config(ctx):
+    click.echo("Welcome to config setup!")
+    is_finished = False
+    while not is_finished:
+        login = click.prompt("Please enter your login")
+        password = click.prompt("Please enter your password")
+        click.echo(f"Your login: {login}\nYour password: {password}")
+        is_correct = click.confirm("Is this correct?")
+        if is_correct:
+            is_finished = True
+            set_config_file(ctx.obj["system"], login, password)
+    click.echo("Config setup finished!")
--- etae/utils/utils.py
+++ etae/utils/utils.py
@@ -33,3 +33,36 @@ def get_config_file(system: SYSTEM) -> dict[str, Any] | str:
         case _:
             print("Unsupported operating system, sorry!")
     return config
+
+
+def set_config_file(system: SYSTEM, login: str, password: str):
+    unix_home_path: Path = Path.home()
+    match system:
+        case "Windows":
+            appdata: str = os.getenv("LOCALAPPDATA")
+            file_path: str = os.path.join(appdata, "Anki2/test.toml")
+            config: dict[str, Any] = create_config_file(file_path, login, password)
+        case "Linux":
+            config: dict[str, Any] = create_config_file(
+                str(unix_home_path) + "/.local/share/Anki2/test.toml", login, password
+            )
+        case "Darwin":
+            config: dict[str, Any] = create_config_file(
+                str(unix_home_path) + "/Library/Application Support/Anki2/test.toml",
+                login,
+                password,
+            )
+        case _:
+            print("Unsupported operating system, sorry!")
+    return
+
+
+def create_config_file(path: str, login: str, password: str) -> bool:
+    try:
+        toml.dump(
+            {"user_info": {"login": login, "password": password}}, open(path, "w")
+        )
+        return True
+    except Exception as e:
+        print(f"Error occured: {e}")
+        return False
